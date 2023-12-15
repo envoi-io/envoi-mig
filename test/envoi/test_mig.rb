8ab1116f790e4a426fc281aaab33123844bd7b59
John Whitson <john.whitson@gmail.com>
2023-12-14 18:03:07 -0700

    Added test placeholders

--- test/envoi/test_mig.rb
+++ test/envoi/test_mig.rb
@@ -2,12 +2,27 @@
 
 require_relative '../test_helper'
 
-class Envoi::TestMig < Minitest::Test
+class TestEnvoiTestMig < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Envoi::Mig::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
+  # Handles the case where the file is not found
+
+  # Handles bad exiftool executable path
+
+  # Handles bad ffmpeg executable path
+
+  # Handles bad mediainfo executable path
+
+  # Handles good URL as media file path
+
+  # Handles bad URL as media file path
+
+  # Handles file path with spaces
+
+  # Handles file path with unicode characters
+
+  # Handles file path with unicode characters and spaces
+
 end
