90dbc4bd8d6f17f52e5bf1882cd68b5d2c8c489b
John Whitson <john.whitson@gmail.com>
2024-10-29 23:15:12 -0600

    Fixed issue with paring dar

--- lib/envoi/mig/modules/ffprobe.rb
+++ lib/envoi/mig/modules/ffprobe.rb
@@ -1,6 +1,8 @@
 require 'open3'
 require 'shellwords'
-require 'time' # unless defined? Time
+require 'time'
+require 'json'
+require 'logger'
 
 class Ffprobe
   class Movie
@@ -12,100 +14,35 @@ class Ffprobe
       raise ArgumentError, "No such file or directory - '#{path}'" unless File.exist?(path)
 
       @path = path
-
       @ffprobe_cmd_path = options.fetch(:ffprobe_cmd_path, 'ffprobe')
+      @logger = options.fetch(:logger, Logger.new($stdout))
 
-      # ffprobe will output to stderr
-      @command = [@ffprobe_cmd_path, '-i', path].shelljoin
-      @output = Open3.popen3(command) { |_stdin, _stdout, stderr| stderr.read }
-
-      fix_encoding(@output)
-
-      output[/Duration: (\d{2}):(\d{2}):(\d{2}\.\d{2})/]
-      @duration = (::Regexp.last_match(1).to_i * 60 * 60) + (::Regexp.last_match(2).to_i * 60) + ::Regexp.last_match(3).to_f
-
-      output[/start: (\d*\.\d*)/]
-      @time = ::Regexp.last_match(1) ? ::Regexp.last_match(1).to_f : 0.0
-
-      output[/creation_time +: +(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})/]
-      @creation_time = ::Regexp.last_match(1) ? Time.parse(::Regexp.last_match(1).to_s) : nil
-
-      output[/bitrate: (\d*)/]
-      @bitrate = ::Regexp.last_match(1) ? ::Regexp.last_match(1).to_i : nil
-
-      output[/rotate +: +(\d*)/]
-      @rotation = ::Regexp.last_match(1) ? ::Regexp.last_match(1).to_i : nil
-
-      output[/Video: (.*)/]
-      @video_stream = ::Regexp.last_match(1)
-
-      output[/Audio: (.*)/]
-      @audio_stream = ::Regexp.last_match(1)
-
-      output[/timecode .* : (.*)/]
-      @timecode = ::Regexp.last_match(1)
-
-      if video_stream.is_a? String
-        # rubocop:disable Style/RescueModifier
-        @video_codec, @colorspace, @resolution, video_bitrate = video_stream&.split(/\s?,\s?/)
-        @video_bitrate = video_bitrate =~ %r{\A(\d+) kb/s\Z} ? ::Regexp.last_match(1).to_i : nil
-        @resolution, aspect_ratios = @resolution.strip.split(' ', 2) rescue @resolution = aspect_ratios = nil
-        @width, @height = @resolution.split('x') rescue @width = @height = nil
-        @frame_rate = ::Regexp.last_match(1) if video_stream[/(\d*\.?\d*)\s?fps/]
-        if aspect_ratios
-          @dar = ::Regexp.last_match(1) if aspect_ratios[/DAR (\d+:\d+)/] rescue nil # Display Aspect Ratio = SAR * PAR
-          @sar = ::Regexp.last_match(1) if aspect_ratios[/SAR (\d+:\d+)/] rescue nil # Storage Aspect Ratio = DAR/PAR
-          @par = ::Regexp.last_match(1) if aspect_ratios[/PAR (\d+:\d+)/] rescue nil # Pixel Aspect Ratio = DAR/SAR
-        end
-        # rubocop:enable Style/RescueModifier
-
-        is_widescreen?
-
-        is_high_definition?
-      end
-
-      if audio_stream.is_a? String
-        @audio_codec, audio_sample_rate, @audio_channels, _unused, audio_bitrate = audio_stream&.split(/\s?,\s?/)
-        @audio_bitrate = audio_bitrate =~ %r{\A(\d+) kb/s\Z} ? ::Regexp.last_match(1).to_i : nil
-        @audio_sample_rate = audio_sample_rate[/\d*/].to_i
-      end
-
-      @invalid = true if @video_stream.to_s.empty? && @audio_stream.to_s.empty?
-      @invalid = true if output.include?('is not supported')
-      @invalid = true if output.include?('could not find codec parameters')
+      run_ffprobe
+      parse_json_output(@output)
     end
 
-    # @return [Boolean]
     def valid?
       !@invalid
     end
 
-    # @return [Boolean?]
     def is_widescreen?
-      @is_widescreen ||= @aspect_quotient_from_dimensions.is_a?(Numeric) ? @aspect_quotient_from_dimensions > 1.4 : nil
+      @is_widescreen ||= aspect_quotient_from_dimensions.is_a?(Numeric) ? aspect_quotient_from_dimensions > 1.4 : nil
     end
     alias is_wide_screen? is_widescreen?
 
-    # Use width instead of height because all standard def resolutions have a width of 720 or less
-    # whereas some high def resolutions have a height ~720 such as 698
-    #
-    # @return [Boolean]
     def is_high_definition?
       @is_high_definition ||= @width.to_i > 720
     end
     alias is_high_def? is_high_definition?
 
-    # Will attempt to
     def calculated_aspect_quotient
       @calculated_aspect_quotient ||= aspect_quotient_from_dar || aspect_quotient_from_dimensions
     end
 
-    # @return [Integer] File Size
     def size
       @size ||= File.size(@path)
     end
 
-    # @return [Integer]
     def audio_channel_count(audio_channels = @audio_channels)
       return 0 unless audio_channels
       return 1 if audio_channels['mono']
@@ -113,7 +50,6 @@ class Ffprobe
       return 6 if audio_channels['5.1']
       return 9 if audio_channels['7.2']
 
-      # If we didn't hit a match above then find any number in #.# format and add them together to get a channel count
       audio_channels[/(\d+.?\d?).*/]
       begin
         audio_channels = ::Regexp.last_match(1).to_s.split('.').sum(&:to_i) if ::Regexp.last_match(1)
@@ -125,8 +61,6 @@ class Ffprobe
       0
     end
 
-    # Outputs relevant instance variables names and values as a hash
-    # @return [Hash]
     def to_hash
       hash = {}
       variables = instance_variables
@@ -141,44 +75,93 @@ class Ffprobe
 
     protected
 
-    # @return [Float|Integer|nil]
     def aspect_quotient_from_dar
       return @aspect_quotient_from_dar if @aspect_quotient_from_dar
       return nil unless dar
 
-      w, h = dar&.split(':')
+      w, h = dar&.split(':')&.map(&:to_i)
       aspect = w.fdiv(h)
       @aspect_quotient_from_dar = aspect.zero? ? nil : aspect
     end
 
-    # @return [Fixed]
     def aspect_quotient_from_dimensions
       return @aspect_quotient_from_dimensions if @aspect_quotient_from_dimensions
       return nil unless width.is_a?(Numeric) && height.is_a?(Numeric)
 
-      aspect = width&.fdiv(height) rescue nil # rubocop:disable Style/RescueModifier
+      aspect = width&.fdiv(height) rescue nil
       @aspect_quotient_from_dimensions = !aspect || aspect.nan? ? nil : aspect
     end
 
-    # @param [String] output
+    private
+
+    def run_ffprobe
+      @command = [
+        @ffprobe_cmd_path,
+        '-v', 'quiet',
+        '-print_format', 'json',
+        '-show_format',
+        '-show_streams',
+        # '-i',
+        @path
+      ].shelljoin
+
+      @output = Open3.popen3(@command) { |_stdin, stdout, _stderr| stdout.read }
+      fix_encoding(@output)
+    rescue StandardError => e
+      @logger.error("Failed to run ffprobe: #{e.message}")
+      @invalid = true
+    end
+
+    def parse_json_output(output_raw)
+      output = JSON.parse(output_raw)
+      format = output['format']
+      tags = format['tags']
+      video_stream = output['streams'].find { |s| s['codec_type'] == 'video' }
+      audio_stream = output['streams'].find { |s| s['codec_type'] == 'audio' }
+
+      @duration = format['duration'].to_f
+      @time = format['start_time'].to_f
+      @creation_time = tags['creation_time']
+      @bitrate = format['bit_rate'].to_i
+      @rotation = video_stream['tags']['rotate'].to_i if video_stream && video_stream['tags'] && video_stream['tags']['rotate']
+
+      if video_stream
+        @video_codec = video_stream['codec_name']
+        @colorspace = video_stream['color_space']
+        @resolution = "#{video_stream['width']}x#{video_stream['height']}"
+        @width = video_stream['width']
+        @height = video_stream['height']
+        @dar = video_stream['display_aspect_ratio']
+        @sar = video_stream['sample_aspect_ratio']
+        @video_bitrate = video_stream['bit_rate'].to_i
+        @frame_rate = video_stream['avg_frame_rate']
+      end
+
+      if audio_stream
+        @audio_codec = audio_stream['codec_name']
+        @audio_sample_rate = audio_stream['sample_rate'].to_i
+        @audio_channels = audio_stream['channels']
+        @audio_bitrate = audio_stream['bit_rate'].to_i
+      end
+    rescue JSON::ParserError => e
+      @logger.error("Failed to parse JSON output: #{e.message}")
+      @invalid = true
+    end
+
     def fix_encoding(output)
-      output[/test/] # Running a regexp on the string throws error if it's not UTF-8
+      output[/test/]
     rescue ArgumentError
       output.force_encoding('ISO-8859-1')
     end
   end
 
-  # @param [Hash] options
-  # @option options [String] :ffprobe_cmd_path
   def initialize(options = {})
     @ffprobe_cmd_path = options.fetch(:ffprobe_cmd_path, 'ffprobe')
+    @logger = options.fetch(:logger, Logger.new($stdout))
   end
 
-  # @param [String] file_path
-  # @param [Hash] options
-  # @option options [String] :ffprobe_cmd_path
   def run(file_path, options = {})
-    options = { ffprobe_cmd_path: @ffprobe_cmd_path }.merge(options)
+    options = { ffprobe_cmd_path: @ffprobe_cmd_path, logger: @logger }.merge(options)
     Movie.new(file_path, options).to_hash
   end
 end
